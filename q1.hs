24647b3f33c806c09158389fddf8826f7fdcfb1d
Ben Gamari <bgamari.foss@gmail.com>
2013-04-02 01:23:06 -0400

    q1: More work

--- q1.hs
+++ q1.hs
@@ -14,6 +14,7 @@ import Numeric.AD hiding (gradientDescent)
 import Numeric.AD.Internal.Reverse
 import Numeric.AD.Mode.Reverse
 import Numeric.AD.Types (lowerFU, auto)
+import Optimization.LineSearch
 
 import Debug.Trace
 
@@ -75,22 +76,25 @@ instance Applicative Config where
     Config x1 y1 z1 <*> Config x2 y2 z2 =
         Config ((<*>) <$> x1 <*> x2) (y1 <*> y2) (z1 <*> z2)
 instance Additive Config where zero = pure 0
+instance Metric Config
 
+-- | To be minimized
 objective :: Fractional a => Config a -> a
-objective (Config {..}) = f2 + f3 - f1
+objective (Config {..}) = negate $ f2 + f3 - f1
   where f1 = ps `dot` ys
         f2 = p's `dot` zs
-        f3 = es `dot` (es ^-^ xs')
-        xs' = fmap F.sum xs
+        f3 = es `dot` (es ^-^ fmap F.sum xs)
 
-p0 = Config { xs = Raws (pure 1) (pure 2) (pure 3) (pure 4)
-            , ys = Raws 1 2 3 4
-            , zs = Blends 1 2 3
+p0 = Config { xs = Raws (pure 100) (pure 100) (pure 100) (pure 100)
+            , ys = Raws 1000 1000 1000 1000
+            , zs = Blends 1000 1000 1000
             }
 
 main = do
     print $ objective p0
-    forM_ (optimize p0) $ \p->print (objective p, p)
+    let p0' = project p0
+    print $ objective p0'
+    forM_ (optimize p0') $ \p->print (objective p, p)
 
 newtype FU f a = FU (forall s. Reifies s Tape => f (Reverse a s) -> Reverse a s)
 
@@ -101,7 +105,7 @@ constraints =
  ++ [ FU $ \(Config {..})->min 0 $ b1 $ ds ^-^ zs
     , FU $ \(Config {..})->min 0 $ b3 $ ds ^-^ zs
     ]
- ++ map (\(FU b)->FU $ \(Config {..})->min 0 $ b rs * b zs - (os `dot` fmap b xs)) fubs
+ ++ map (\(FU b)->FU $ \(Config {..})->min 0 $ negate $ b rs * b zs - (os `dot` fmap b xs)) fubs
  ++ map (\(FU b)->FU $ \(Config {..})->min 0 $ b $ r1 xs) fubs
  ++ map (\(FU b)->FU $ \(Config {..})->min 0 $ b $ r2 xs) fubs
  ++ map (\(FU b)->FU $ \(Config {..})->min 0 $ b $ r3 xs) fubs
@@ -115,20 +119,25 @@ project :: (Fractional a, Ord a, Show a) => Config a -> Config a
 project c@(Config {..}) =
     case unmet of
       []          -> c
-      otherwise   -> traceShow (length unmet, c) $ project
-                     $ foldl' fixConstraint c unmet
-  where unmet = filter (\(FU constr)->let (y,_) = grad' constr c in y /= 0) constraints
-        fixConstraint c (FU constr) = head $ drop 50 $ gradientDescent (FU $ (^2) . constr) c
+      otherwise   -> --traceShow (length unmet, c) $
+                     project $ foldl' fixConstraint c unmet
+  where unmet = filter (not . met c) constraints
+        met c (FU constr) = let (y,_) = grad' constr c in abs y < 1
+        fixConstraint c (FU constr) = head $ dropWhile (\c'->not $ met c' $ FU constr)
+                                      $ gradientDescent (FU $ (^2) . constr) c
 
-projGradientDescent :: (Additive f, Traversable f, Ord a, Fractional a)
+-- | Minimize the given objective
+projGradientDescent :: (Additive f, Traversable f, Metric f, Ord a, Fractional a)
                     => (f a -> f a) -> FU f a -> f a -> [f a]
 projGradientDescent proj (FU obj) = go
-  where go x0 = let alpha0 = 0.1
-                    x1 = proj $ x0 ^-^ alpha0 *^ df x0
+  where go x0 = let p = negated $ df x0
+                    alpha0 = armijoSearch 0.1 0.2 100 f df p x0
+                    f x = let (y,_) = grad' obj x in y
+                    x1 = proj $ x0 ^+^ alpha0 *^ p
                 in x1 : go x1
         df = grad obj
 
-gradientDescent :: (Additive f, Traversable f, Ord a, Fractional a)
+gradientDescent :: (Additive f, Traversable f, Metric f, Ord a, Fractional a)
                 => FU f a -> f a -> [f a]
 gradientDescent = projGradientDescent id
 
